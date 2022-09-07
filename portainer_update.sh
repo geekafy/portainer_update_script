8da6b73de16f3065cf1e2cad30a0e58a904d69d9
Matthew <git@vixitconsulting.ca>
2022-09-07 12:56:59 -0400

    Add files via upload

---
+++ portainer_update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------
+# [Author] Matthew Bernier (@ggkafy)
+# [Description] My shell script to update Portainer on server
+# [Version] 1.0
+
+# ------------------------------------------------------------------
+
+sudo apt-get update && sudo apt-get upgrade -y
+docker stop portainer
+docker rm portainer
+docker pull portainer/portainer-ce:latest
+docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+clr
