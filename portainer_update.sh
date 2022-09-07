03a1c836846d8882574510e8525bb515d03e9b44
Matthew <git@vixitconsulting.ca>
2022-09-07 12:59:57 -0400

    Update portainer_update.sh

--- portainer_update.sh
+++ portainer_update.sh
@@ -3,7 +3,7 @@
 # [Author] Matthew Bernier (@ggkafy)
 # [Description] My shell script to update Portainer on server
 # [Version] 1.0
-# [Repo Location]   
+# [Repo Location] https://github.com/geekafy/portainer_update_script/blob/43a8054e589a66f9bd734df446de6a37f5d1ff11/portainer_update.sh
 # ------------------------------------------------------------------
 
 sudo apt-get update && sudo apt-get upgrade -y
@@ -12,3 +12,4 @@ docker rm portainer
 docker pull portainer/portainer-ce:latest
 docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 clr
+ECHO "Portainer updated."
